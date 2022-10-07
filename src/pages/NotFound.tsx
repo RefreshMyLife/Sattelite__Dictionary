c58282a207e7b7c84a8f6b810d132a23d0b14236
Andrey Zaharov <andreuzakharoy777@gmail.com>
2022-10-07 04:07:20 +0300

    fix NotFound page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound: React.FC = () => {
   return (
     <div>
-      <h1>No Definitions Found</h1>
+      <h1>No Definitions Found :(</h1>
       <h2>Sorry pal, we couldn't find definitions for the word you were looking for.</h2>
     </div>
   );
